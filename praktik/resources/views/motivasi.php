86f3ffed21c90b2b91f14da57c3ce74e47362268
09020582327024-NyayuMiftahulJannah <nyayumiftahuljannah@gmail.com>
2025-04-21 03:30:48 +0000

    motivasi

---
+++ praktik/resources/views/motivasi.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Yang Asli</title>
+</head>
+<body>
+    <h1>Yang Asli</h1>
+    <p>
+        Hari ini, kurangilah hal-hal yang palsu dalam kehidupan mu. Agar rezeki \
+        & cinta yang datang lebih asli. - Mario Teguh
+    </p>
+</body>
+</html>
